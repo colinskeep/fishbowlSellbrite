3c6ec874de741148f5c1f19bace2c6ddf19c1cb7
Colin Inskeep <colinjinskeep@gmail.com>
2019-01-04 16:19:35 -0700

    various fixes

--- mapSalesOrder4.js
+++ mapSalesOrder4.js
@@ -13,8 +13,7 @@ exports.get = async function mapSalesOrder(data){
         var fbaarr = [];
         const [rows,fields] = await db.execute(`SELECT sku FROM fba`);
         const location = await isFba.get(rows, data[i].items[0].sku);
-        if(data[i].shipping_country_code === 'MY' || data[i].shipping_country_code === 'SE' || data[i].shipping_country_code === 'NZ' || data[i].shipping_country_code == 'IL' || data[i].shipping_country_code == 'GR'  || data[i].shipping_country_code == 'PR' || data[i].shipping_country_code === 'DE'){
-          //data[i].shipping_country_code = 'ZZ';
+        if(data[i].shipping_country_code !== 'US'){
           data[i].shipping_state_region = '';
         }
         if(data[i].display_ref.length > 24) {
@@ -23,23 +22,25 @@ exports.get = async function mapSalesOrder(data){
         if (data[i].shipping_address_1 == null){
           data[i].shipping_address_1 = data[i].shipping_address_2
         }
-        if(data[i].display_ref.startsWith('S') == false && data[i].display_ref != '97157-2790026192970' && data[i].channel_name != 'Walmart' && data[i].items[0].unit_price !== 0){
+        if(data[i].display_ref.startsWith('S') == false && data[i].channel_name != 'Walmart' && data[i].items[0].inventory_sku != null && data[i].items[0].unit_price !== 0){
           if(socount == 0){
             arr =  [];
             arr.push('"Flag","SONum","Status","CustomerName","CustomerContact","BillToName","BillToAddress","BillToCity","BillToState","BillToZip","BillToCountry","ShipToName","ShipToAddress","ShipToCity","ShipToState","ShipToZip","ShipToCountry","ShipToResidential","CarrierName","TaxRateName","PriorityId","PONum","VendorPONum","Date","Salesman","ShippingTerms","PaymentTerms","FOB","Note","QuickBooksClassName","LocationGroupName","FulfillmentDate","URL","CarrierService","DateExpired","Phone","Email","CF-Custom"');
             arr.push('"Flag","SOItemTypeID","ProductNumber","ProductDescription","ProductQuantity","UOM","ProductPrice","Taxable","TaxCode","Note","QuickBooksClassName","FulfillmentDate","ShowItem","KitItem","RevisionLevel"');
           }
-          console.log(data[i])
           arr.push('"SO","'+data[i].display_ref+'",20,"'+data[i].channel_name+'","'+diacritics.remove(data[i].shipping_contact_name.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_contact_name.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_address_1.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_city.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_state_region.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+data[i].shipping_postal_code+'","'+data[i].shipping_country_code+'","'+diacritics.remove(data[i].shipping_contact_name.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_address_1.replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_city.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+diacritics.remove(data[i].shipping_state_region.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,''))+'","'+data[i].shipping_postal_code+'","'+data[i].shipping_country_code+'","FALSE","Will Call","None","30","'+data[i].display_ref+'","'+data[i].display_ref+'","'+date+'","admin","Prepaid","COD","Origin",,"None","'+location+'",,,,,,,')
           socount++
           for(let x = 0; x < data[i].items.length; x++){
             arr.push('"Item","10","'+data[i].items[x].inventory_sku+'","'+data[i].items[x].title.replace(/,/g, '').replace(/"/g, '').replace(/'/g, '').replace(/\./g,'')+'","'+data[i].items[x].quantity+'","ea","'+data[i].items[x].unit_price+'","TRUE","NON",,"None",,"TRUE","FALSE"');
         }
-      } else {console.log("did not import order: ", data[i].display_ref, "at :", data[i].items[0].unit_price)}
+      } else {
+        if (data[i].items[0].inventory_sku == null  && data[i].display_ref.startsWith('S') == false){
+          console.log(data[i].display_ref, data[i].items[0].sku, " needs to be mapped")
+        }
+      }
       if(socount == 10 || i== data.length -1){
           console.log("attempting to input ",socount," orders");
           socount = 0;
-          console.log(arr);
           return(arr);
        }
      }catch(err){console.log(err)}
